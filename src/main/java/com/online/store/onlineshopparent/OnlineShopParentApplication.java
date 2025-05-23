fa752c7c12f84d405908ae3b457965977553fc70
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-23 15:52:26 +0400

    first commit

---
+++ src/main/java/com/online/store/onlineshopparent/OnlineShopParentApplication.java
@@ -0,0 +1,13 @@
+package com.online.store.onlineshopparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/online/store/onlineshopparent/OnlineShopParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.store.onlineshopparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
