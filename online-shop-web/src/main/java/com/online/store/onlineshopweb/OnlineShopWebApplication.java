1068deb4fcb0811e6618db039a5cfcf57e48a906
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-26 23:16:53 +0400

    changes

--- online-shop-web/src/main/java/com/online/store/onlineshopweb/OnlineShopWebApplication.java
+++ online-shop-web/src/main/java/com/online/store/onlineshopweb/OnlineShopWebApplication.java
@@ -2,8 +2,14 @@ package com.online.store.onlineshopweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.online.store.onlineshopweb", "com.online.store.onlineshopcommon"})
+@EntityScan( basePackages = "com.online.store.onlineshopcommon.entity")
+@EnableJpaRepositories(basePackages = "com.online.store.onlineshopcommon.repository")
 public class OnlineShopWebApplication {
 
     public static void main(String[] args) {
---
+++ online-shop-web/src/main/java/com/online/store/onlineshopweb/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.online.store.onlineshopweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "index";
+    }
+
+}
