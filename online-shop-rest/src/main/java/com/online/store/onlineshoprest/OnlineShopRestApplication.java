29285082be5e6cfdfc7ba07dace3aa37d7758a0b
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-26 15:48:18 +0400

    app changes

--- online-shop-rest/src/main/java/com/online/store/onlineshoprest/OnlineShopRestApplication.java
+++ online-shop-rest/src/main/java/com/online/store/onlineshoprest/OnlineShopRestApplication.java
@@ -2,7 +2,13 @@ package com.online.store.onlineshoprest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan(basePackages = {"com.online.store.onlineshoprest", "com.online.store.onlineshopcommon"})
+@EntityScan( basePackages = "com.online.store.onlineshopcommon.entity")
+@EnableJpaRepositories(basePackages = "com.online.store.onlineshopcommon.repository")
 @SpringBootApplication
 public class OnlineShopRestApplication {
 
