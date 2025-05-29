f1d95511f4044a92a8b2168cf8814164370bbf9b
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-29 14:18:04 +0400

    shop controller

---
+++ online-shop-web/src/main/java/com/online/store/onlineshopweb/controller/ShopController.java
@@ -0,0 +1,14 @@
+package com.online.store.onlineshopweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "shop";
+    }
+
+}
