a6482472437c901cd917f35a165364b355d4e5c4
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-29 20:13:46 +0400

    shop controller modified

--- online-shop-web/src/main/java/com/online/store/onlineshopweb/controller/ShopController.java
+++ online-shop-web/src/main/java/com/online/store/onlineshopweb/controller/ShopController.java
@@ -1,14 +1,30 @@
 package com.online.store.onlineshopweb.controller;
 
+import com.online.store.onlineshopcommon.entity.Product;
+import com.online.store.onlineshopcommon.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ShopController {
 
+    @Autowired
+    public ShopController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    private ProductService productService;
+
     @GetMapping("/shop")
-    public String shop() {
+    public String getShopPage(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
         return "shop";
     }
+
 
 }
