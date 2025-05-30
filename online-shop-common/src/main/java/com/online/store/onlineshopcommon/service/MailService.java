105eb5ec4033328fcd13ebc10787d52f951b66dd
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-30 14:03:38 +0400

    mail service

---
+++ online-shop-common/src/main/java/com/online/store/onlineshopcommon/service/MailService.java
@@ -0,0 +1,23 @@
+package com.online.store.onlineshopcommon.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    private final MailSender mailSender;
+
+    @Async
+    public void sendMail(String to, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
+}
