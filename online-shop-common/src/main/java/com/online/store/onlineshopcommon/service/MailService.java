0c03a23c79f518b3423da7ce95e4d0e478b7fc85
Tamara Murazyan <murazyan.tamara5566.com>
2025-06-02 22:21:54 +0400

    changes
    (cherry picked from commit 27a379703b69ecfa8649f99a90eab0e964405799)

--- online-shop-common/src/main/java/com/online/store/onlineshopcommon/service/MailService.java
+++ online-shop-common/src/main/java/com/online/store/onlineshopcommon/service/MailService.java
@@ -1,15 +1,23 @@
 package com.online.store.onlineshopcommon.service;
 
+import com.online.store.onlineshopcommon.entity.User;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
 public class MailService {
-    private final MailSender mailSender;
+
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
 
     @Async
     public void sendMail(String to, String subject, String message) {
@@ -17,7 +25,28 @@ public class MailService {
         mailMessage.setTo(to);
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
-
         mailSender.send(mailMessage);
+    }
+
+    @Async
+    public void sendWelcomeMail(User user) {
+        final Context ctx = new Context();
+        ctx.setVariable("user", user.getUsername());
+
+        final String htmlContent = templateEngine.process("mail/welcome.html", ctx);
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+
+             final MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+            message.setSubject("Welcome to Our Online Store!");
+            message.setTo(user.getEmail());
+
+            message.setText(htmlContent, true);
+
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send welcome email", e);
+        }
     }
 }
