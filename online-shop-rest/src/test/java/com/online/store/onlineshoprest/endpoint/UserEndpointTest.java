5ba745ee33f28230b5911fd3e24b027d207f3e1a
Tamara Murazyan <murazyan.tamara5566.com>
2025-06-05 11:35:06 +0400

    modified

--- online-shop-rest/src/test/java/com/online/store/onlineshoprest/endpoint/UserEndpointTest.java
+++ online-shop-rest/src/test/java/com/online/store/onlineshoprest/endpoint/UserEndpointTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+//@ActiveProfiles("integration")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserEndpointTest {
 
