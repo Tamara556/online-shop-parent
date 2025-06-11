3c9a44a261816f138d35161f961beb49f6b8920d
Tamara Murazyan <murazyan.tamara5566.com>
2025-06-11 14:18:46 +0400

    cors changes

--- online-shop-rest/src/main/java/com/online/store/onlineshoprest/config/CorsConfig.java
+++ online-shop-rest/src/main/java/com/online/store/onlineshoprest/config/CorsConfig.java
@@ -11,22 +11,6 @@ import static org.springframework.web.cors.CorsConfiguration.ALL;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
 
-//    private static final String All = "*";
-//
-//    @Value("${app.cors.prigins}")
-//    private String origin;
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(ALL)
-//                .allowedMethods(ALL)
-//                .allowedHeaders(All)
-//                .exposedHeaders(
-//                        "X-Total-Count"
-//                );
-//    }
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -36,6 +20,7 @@ public class CorsConfig implements WebMvcConfigurer{
                         .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
                         .allowCredentials(true);
             }
         };
---
+++ online-shop-rest/src/main/java/com/online/store/onlineshoprest/config/CorsFilterConfig.java
@@ -0,0 +1,27 @@
+package com.online.store.onlineshoprest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsFilterConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Разрешить Angular
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Применить ко всем маршрутам
+
+        return new CorsFilter(source);
+    }
+}
