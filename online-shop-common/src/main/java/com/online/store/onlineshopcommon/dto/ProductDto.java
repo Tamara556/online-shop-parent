48a17560631b3cd0cb9d31985c7b9f4cde0a48eb
Tamara556 <murazyan.tamara5566@gmail.com>
2025-06-20 11:20:22 +0400

    changes

--- online-shop-common/src/main/java/com/online/store/onlineshopcommon/dto/ProductDto.java
+++ online-shop-common/src/main/java/com/online/store/onlineshopcommon/dto/ProductDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDto {
+    private int id;
     private String name;
     private Integer price;
     private String description;
